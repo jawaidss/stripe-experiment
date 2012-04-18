b9105cb976659d6b1a17d80facb5f5a951e688ab
Samad Jawaid <jawaidss@rose-hulman.edu>
2012-04-18 19:43:33 -0400

    Add messages

--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
+    messages.info(request, 'This is an info message.')
+    messages.success(request, 'This is a success message.')
+    messages.warning(request, 'This is a warning message.')
+    messages.error(request, 'This is an error message.')
     return render_to_response('main/index.html',
                               RequestContext(request))
